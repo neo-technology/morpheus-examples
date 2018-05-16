f97858e7543921115b1ed4bf7f48b22689119577
Mats Rydberg <mats@neotechnology.com>
2018-05-16 16:09:45 +0200

    Add SQL DDL example based on the Northwind dataset
    - JDBC version, using in-mem H2 database
    - All properties are upper-case due to LIKE
    
    Co-Authored-By: Philip Stutz <philip.stutz@gmail.com>

---
+++ src/main/scala/org/neo4j/morpheus/examples/NorthwindSQLExampleJDBC.scala
@@ -0,0 +1,47 @@
+package org.neo4j.morpheus.examples
+
+import java.nio.file.{Path, Paths}
+
+import org.neo4j.morpheus.utils.NorthwindDB
+import org.neo4j.sql.SqlGraphSource
+import org.opencypher.okapi.api.graph.Namespace
+import org.opencypher.spark.api.CAPSSession
+
+object NorthwindSQLExampleJDBC extends App {
+
+  // start up the SQL database
+  NorthwindDB.init()
+
+  // Initialise local CAPS session
+  implicit val session: CAPSSession = CAPSSession.local()
+
+  // define the root configuration directory for the SQL graph source
+  // this holds the data source mappings files and the SQL DDL file
+  // the latter contains the graph definitions and mappings from SQL tables that fill the graph with data
+  private val rootDir: Path = Paths.get(getClass.getResource("/sql/pgds").toURI)
+
+  // register the SQL graph source with the session
+  session.registerSource(Namespace("sql"), SqlGraphSource(rootDir, "northwind.ddl.sql", "h2-source.json"))
+
+  // print the number of nodes in the graph
+  session.cypher(
+    """
+      |FROM GRAPH sql.Northwind
+      |MATCH (n)
+      |RETURN count(n)
+    """.stripMargin).show
+
+  // print the schema of the graph
+  println(session.catalog.graph("sql.Northwind").schema.pretty)
+
+  // run a simple query
+  session.cypher(
+    """
+      |FROM GRAPH sql.Northwind
+      |MATCH (e:Employee)-[:REPORTS_TO]->(:Employee)<-[:HAS_EMPLOYEE]-(o:Order)
+      |RETURN o.CUSTOMERID, o.ORDERDATE, e.TITLE
+      |  ORDER BY o.ORDERDATE
+      |  LIMIT 50
+    """.stripMargin).show
+
+}
---
+++ src/main/scala/org/neo4j/morpheus/utils/NorthwindHelpers.scala
@@ -0,0 +1,25 @@
+package org.neo4j.morpheus.utils
+
+import scala.io.Source
+
+object NorthwindHelpers {
+
+  private def readResourceAsString(name: String, filterNot: String => Boolean = _ => false): String =
+    Source.fromFile(getClass.getResource(name).toURI)
+      .getLines()
+      .filterNot(_.startsWith("#"))
+      .filterNot(filterNot)
+      .mkString("\n")
+
+  def schemaSql: String = readResourceAsString(sqlSchemaFile, _.startsWith("CREATE INDEX"))
+  def dataSql: String = readResourceAsString(sqlDataFile)
+  def viewSql: String = readResourceAsString(sqlViewFile)
+
+  private val dir = "/sql"
+
+  val sqlSchemaFile = s"$dir/northwind_schema.sql"
+  val sqlViewFile = s"$dir/northwind_views.sql"
+  val sqlDataFile = s"$dir/northwind_data.sql"
+  val sqlDdlFile = s"$dir/northwind.ddl.sql"
+
+}
